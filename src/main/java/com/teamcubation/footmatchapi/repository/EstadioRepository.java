e50b1f1d9dc81695f44ab1a132f7a96f2f649c58
bauermannv <vinicius.balmeida@mercadolivre.com>
2025-07-03 09:50:32 -0300

    adiciona regras de negocio em estadioService

--- src/main/java/com/teamcubation/footmatchapi/repository/EstadioRepository.java
+++ src/main/java/com/teamcubation/footmatchapi/repository/EstadioRepository.java
@@ -1,9 +1,14 @@
 package com.teamcubation.footmatchapi.repository;
 
 import com.teamcubation.footmatchapi.domain.entities.Estadio;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface EstadioRepository extends JpaRepository<Estadio, Long> {
 
 
+    Optional<Estadio> findByNome(@NotBlank(message = "O nome do estádio é obrigatório.") @Size(min = 3, message = "O nome deve conter ao menos 3 caracteres.") String nome);
 }
--- src/main/java/com/teamcubation/footmatchapi/service/EstadioService.java
+++ src/main/java/com/teamcubation/footmatchapi/service/EstadioService.java
@@ -1,21 +1,39 @@
 package com.teamcubation.footmatchapi.service;
 
+import com.teamcubation.footmatchapi.domain.entities.Estadio;
 import com.teamcubation.footmatchapi.dto.request.EstadioRequestDTO;
 import com.teamcubation.footmatchapi.dto.response.EstadioResponseDTO;
+import com.teamcubation.footmatchapi.mapper.EstadioMapper;
 import com.teamcubation.footmatchapi.repository.EstadioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class EstadioService {
 
     public final EstadioRepository estadioRepository;
+    private final EstadioMapper estadioMapper;
 
     public EstadioResponseDTO criarEstadio(EstadioRequestDTO estadioRequestDTO) {
-        return null;
+
+        if (estadioRequestDTO.getNome() == null || estadioRequestDTO.getNome().trim().length() < 3) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O nome do estádio deve ter pelo menos 3 letras.");
+        }
+
+        Optional<Estadio> estadioExistente = estadioRepository.findByNome (estadioRequestDTO.getNome());
+        if (estadioExistente.isPresent()){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Já existe um estádio com esse nome.");
+        }
+
+        Estadio estadio = estadioMapper.toEntity(estadioRequestDTO);
+        Estadio salvo = estadioRepository.save(estadio);
+        return estadioMapper.toDto(salvo);
     }
 
     public List<EstadioResponseDTO> obterEstadios() {
@@ -23,11 +41,28 @@ public class EstadioService {
     }
 
     public EstadioResponseDTO obterEstadioPorId(Long id) {
-        return null;
+        Estadio estadio = estadioRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Estádio não encontrado."));
+        return estadioMapper.toDto(estadio);
     }
 
     public EstadioResponseDTO atualizarEstadio(Long id, EstadioRequestDTO estadioRequestDTO) {
-        return null;
+
+        Estadio estadio = estadioRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Estádio não encontrado."));
+
+        if (estadioRequestDTO.getNome() == null || estadioRequestDTO.getNome().trim().length() < 3) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O nome do estádio deve ter pelo menos 3 letras.");
+        }
+
+        Optional<Estadio> estadioExistente = estadioRepository.findByNome(estadioRequestDTO.getNome());
+        if (estadioExistente.isPresent() && !estadioExistente.get().getId().equals(id)) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Já existe um estádio com esse nome.");
+        }
+
+        estadio.setNome(estadioRequestDTO.getNome());
+        Estadio salvo = estadioRepository.save(estadio);
+        return estadioMapper.toDto(salvo);
     }
 
     public void deletarEstadio(Long id) {
