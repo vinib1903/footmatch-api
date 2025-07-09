2df31943ac2bbd33a62112070af245898e2ae526
vinib1903 <vinib1903@gmail.com>
2025-07-09 13:36:04 -0300

    ordena estados no enum SiglaEstado

--- src/main/java/com/teamcubation/footmatchapi/domain/enums/SiglaEstado.java
+++ src/main/java/com/teamcubation/footmatchapi/domain/enums/SiglaEstado.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public enum SiglaEstado {
 
-    AC, AL, AP, AM, BA, CE, DF, ES, GO,
-    MT, MS, MG, PA, PB, PR, PE, PI, RJ,
-    RS, RO, RR, SC, SP, SE, TO, MA, RN;
+    AC, AL, AM, AP, BA, CE, DF, ES, GO,
+    MA, MG, MS, MT, PA, PB, PE, PI, PR,
+    RJ, RN, RO, RR, RS, SC, SE, SP, TO;
 
     public static boolean isValido(String siglaEstado) {
         return Arrays.stream(values()).anyMatch(e -> e.name().equalsIgnoreCase(siglaEstado));
