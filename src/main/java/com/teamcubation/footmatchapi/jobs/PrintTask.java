fc9973f542e5bca504d5f0be4574f792c36db59e
vinib1903 <vinib1903@gmail.com>
2025-08-05 10:50:11 -0300

    altera task agendada para ver funcionamento do delay

--- src/main/java/com/teamcubation/footmatchapi/jobs/PrintTask.java
+++ src/main/java/com/teamcubation/footmatchapi/jobs/PrintTask.java
@@ -12,12 +12,13 @@ public class PrintTask {
     @Scheduled(fixedRate = 5000)
     @Async
     public void printMessageRate() {
-        System.out.println("Essa mensagem sera exibida a cada 5 segundos - " + System.currentTimeMillis());
+        System.out.println("Essa mensagem sera exibida a cada 5 segundos - " + LocalDateTime.now());
     }
 
     @Scheduled(fixedDelay = 10000)
-    public void printMessageDelay() {
-        System.out.println("Essa mensagem sera exibida a cada 10 segundos, após a execução da anterior - " + System.currentTimeMillis());
+    public void printMessageDelay() throws InterruptedException {
+        System.out.println("Essa mensagem sera exibida a cada 10 segundos, após a execução da anterior - " + LocalDateTime.now());
+        Thread.sleep(5000);
     }
 
     @Scheduled(cron = "0 0/2 16 * * *")
