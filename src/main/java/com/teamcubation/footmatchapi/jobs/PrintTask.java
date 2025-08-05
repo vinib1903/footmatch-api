e72167b6abdea2d8137a859f2c8c5fc805e9a201
vinib1903 <vinib1903@gmail.com>
2025-08-05 10:04:34 -0300

    adiciona arquivo e config de scheduled tasks

---
+++ src/main/java/com/teamcubation/footmatchapi/jobs/PrintTask.java
@@ -0,0 +1,29 @@
+package com.teamcubation.footmatchapi.jobs;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class PrintTask {
+
+    @Scheduled(fixedRate = 5000)
+    @Async
+    public void printMessageRate() {
+        System.out.println("Essa mensagem sera exibida a cada 5 segundos - " + System.currentTimeMillis());
+    }
+
+    @Scheduled(fixedDelay = 10000)
+    public void printMessageDelay() {
+        System.out.println("Essa mensagem sera exibida a cada 10 segundos, após a execução da anterior - " + System.currentTimeMillis());
+    }
+
+    @Scheduled(cron = "0 0/2 16 * * *")
+    public void printMessageCron() {
+        System.out.println("Essa mensagem sera exibida a cada 2 minutos das 16:00 - " + LocalDateTime.now());
+    }
+
+
+}
